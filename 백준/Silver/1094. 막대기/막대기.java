d00779500868964463e259bd6b60573dc400e882
dkrdj <109264979+dkrdj@users.noreply.github.com>
2022-09-24 01:02:10 +0900

    [Silver V] Title: 막대기, Time: 76 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int X = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for (int i = 0; i < 7; i++) {
+            if ((X & (1 << i)) != 0) cnt++;
+        }
+        System.out.println(cnt);
+    }
+
+}
