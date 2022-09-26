5195cdff7e24c673873cb6f601121d7e076e6936
dkrdj <109264979+dkrdj@users.noreply.github.com>
2022-09-27 00:06:31 +0900

    [Silver II] Title: 스타트와 링크, Time: 208 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,64 +1,68 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
     static int[][] arr;
-    static int N, ans = Integer.MAX_VALUE;
+    static int[] arrA, arrB;
+    static int min;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
-        arr = new int[N][N];
-        for (int i = 0; i < N; i++) {
+        arr = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
+            for (int j = 1; j <= N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        arrA = new int[N / 2 + 1];
+        arrB = new int[N / 2 + 1];
+        min = Integer.MAX_VALUE;
+        comb(1, 1, 0);
 
-        comb(0, 0, 0);
-        System.out.println(ans);
+        System.out.println(min);
     }
 
-    private static void comb(int idx, int sidx, int visited) {
-        if (sidx == N / 2) {
-            calc(visited);
+    static void comb(int idx, int sidx, int visited) {
+        if (sidx == N / 2 + 1) {
+            int ans = calc(visited);
+            min = Math.min(min, ans);
             return;
         }
-        for (int i = idx; i < N; i++) {
+        for (int i = idx; i < N / 2 + sidx; i++) {
+            arrA[sidx] = i;
+            comb(i + 1, sidx + 1, visited | (1 << i));
+        }
+    }
+
+    static int calc(int visited) {
+        for (int i = 1, cnt = 1; i <= N; i++) {
             if ((visited & (1 << i)) == 0) {
-                comb(i + 1, sidx + 1, visited | (1 << i));
+                arrB[cnt++] = i;
             }
         }
-
+        int sumA = 0;
+        int sumB = 0;
+        for (int i = 1; i <= N / 2; i++) {
+            for (int j = 1; j <= N / 2; j++) {
+                if (i != j) {
+                    sumA += arr[arrA[i]][arrA[j]];
+                }
+            }
+        }
+        for (int i = 1; i <= N / 2; i++) {
+            for (int j = 1; j <= N / 2; j++) {
+                if (i != j) {
+                    sumB += arr[arrB[i]][arrB[j]];
+                }
+            }
+        }
+        return Math.abs(sumA - sumB);
     }
 
-    private static void calc(int visited) {
-        List<Integer> team1 = new ArrayList<>();
-        List<Integer> team2 = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            if ((visited & (1 << i)) != 0) {
-                team1.add(i);
-            } else {
-                team2.add(i);
-            }
-        }
-        int abil1 = 0;
-        for (int num1 : team1) {
-            for (int num2 : team1) {
-                abil1 += arr[num1][num2];
-            }
-        }
-        int abil2 = 0;
-        for (int num1 : team2) {
-            for (int num2 : team2) {
-                abil2 += arr[num1][num2];
-            }
-        }
-        ans = Math.min(ans, Math.abs(abil1 - abil2));
-    }
 }
