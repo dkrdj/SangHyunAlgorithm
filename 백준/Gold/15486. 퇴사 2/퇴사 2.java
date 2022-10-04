8ff4feb823963e7f8bf30f1cf10d12b389dc1edd
dkrdj <109264979+dkrdj@users.noreply.github.com>
2022-10-04 19:44:03 +0900

    [Gold V] Title: 퇴사 2, Time: 676 ms, Memory: 314056 KB -BaekjoonHub

---
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 2];
+        int[] t = new int[N + 1];
+        int[] p = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        for (int i = N; i > 0; i--) {
+            if (i + t[i] > N + 1) {
+                dp[i] = dp[i + 1];
+            } else {
+                dp[i] = Math.max(dp[i + 1], p[i] + dp[i + t[i]]);
+            }
+        }
+
+        System.out.println(dp[1]);
+
+    }
+}
