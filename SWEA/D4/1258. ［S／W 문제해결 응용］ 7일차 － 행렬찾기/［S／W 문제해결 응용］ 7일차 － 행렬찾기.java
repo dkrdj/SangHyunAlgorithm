ecd04695680b11f04884c80d986a2e778bb27e71
dkrdj <109264979+dkrdj@users.noreply.github.com>
2022-10-05 21:54:37 +0900

    [D4] Title: [S/W 문제해결 응용] 7일차 - 행렬찾기, Time: 191 ms, Memory: 39,084 KB -BaekjoonHub

--- SWEA/D4/1258. ［S／W 문제해결 응용］ 7일차 － 행렬찾기/［S／W 문제해결 응용］ 7일차 － 행렬찾기.java
+++ SWEA/D4/1258. ［S／W 문제해결 응용］ 7일차 － 행렬찾기/［S／W 문제해결 응용］ 7일차 － 행렬찾기.java
@@ -1,14 +1,13 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
     static int[][] arr;
     static boolean[][] visited;
-    static List<Matrix> list;
+    static PriorityQueue<Matrix> list;
     static int N;
 
     public static void main(String[] args) throws IOException {
@@ -19,29 +18,32 @@ public class Solution {
         for (int t = 1; t <= T; t++) {
             N = Integer.parseInt(br.readLine());
             arr = new int[N][N];
-            visited = new boolean[N][N];
-            list = new ArrayList<>();
+            visited = new boolean[N][N]; // 방문배열
+            list = new PriorityQueue<>((o1, o2) -> { // 출력형식에 맞게 정렬
+                if (o1.r * o1.c == o2.r * o2.c) {
+                    return o1.r - o2.r;
+                }
+                return o1.r * o1.c - o2.r * o2.c;
+            }); // 답을 넣어줄 리스트
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
                     arr[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
+            //여기까지 입력
+
             for (int i = 0; i < N; i++) {
                 for (int j = 0; j < N; j++) {
-                    if (!visited[i][j] && arr[i][j] != 0) {
-                        findMatrix(i, j);
+                    if (!visited[i][j] && arr[i][j] != 0) { // 방문하지 않고 0이 아닌곳이 나오면
+                        findMatrix(i, j); // 크기를 찾기 시작
                     }
                 }
             }
-            list.sort((o1, o2) -> {
-                if (o1.r * o1.c == o2.r * o2.c) {
-                    return o1.r - o2.r;
-                }
-                return o1.r * o1.c - o2.r * o2.c;
-            });
+            //출력하기
             sb.append("#").append(t).append(" ").append(list.size()).append(" ");
-            for (Matrix mtx : list) {
+            while (!list.isEmpty()) {
+                Matrix mtx = list.poll();
                 int r = mtx.r;
                 int c = mtx.c;
                 sb.append(r).append(" ").append(c).append(" ");
@@ -54,18 +56,18 @@ public class Solution {
     private static void findMatrix(int r, int c) {
         int nr = r;
         int nc = c;
-        while (nr + 1 < N && arr[nr + 1][nc] != 0) {
+        while (nr + 1 < N && arr[nr + 1][nc] != 0) { // arr을 벗어나지 않으면서, 0이 아니면 계속 nr을 증가시킴.
             nr++;
         }
-        while (nc + 1 < N && arr[nr][nc + 1] != 0) {
+        while (nc + 1 < N && arr[nr][nc + 1] != 0) {// arr을 벗어나지 않으면서, 0이 아니면 계속 nc을 증가시킴.
             nc++;
         }
-        for (int i = r; i <= nr; i++) {
+        for (int i = r; i <= nr; i++) { // 찾은 nr,nc값을 방문처리 하고
             for (int j = c; j <= nc; j++) {
                 visited[i][j] = true;
             }
         }
-        list.add(new Matrix(nr - r + 1, nc - c + 1));
+        list.add(new Matrix(nr - r + 1, nc - c + 1)); // 답을 리스트에 집어넣음.
     }
 
     private static class Matrix {
@@ -78,4 +80,3 @@ public class Solution {
         }
     }
 }
-
