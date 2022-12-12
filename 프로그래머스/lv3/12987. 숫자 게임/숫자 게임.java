e6f1002cfea660d471415722711836dde6b2b777
dkrdj <109264979+dkrdj@users.noreply.github.com>
2022-12-12 09:41:35 +0900

    [level 3] Title: 숫자 게임, Time: 56.56 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.java
@@ -5,10 +5,9 @@ class Solution {
         int answer = 0;
         Arrays.sort(A);
         Arrays.sort(B);
-        for(int i = 0, j = 0 ; i < A.length && j < B.length; i++, j++){
-            if(B[j]> A[i]) answer++;
+        for(int i = 0, j = 0 ; i < A.length && j < B.length; i++, j++)
+            if(B[j] > A[i]) answer++;
             else i--;
-        }
         return answer;
     }
 }
