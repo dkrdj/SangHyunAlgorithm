e84fdbe2ef8fee0d8a054f21be074da8d94442e5
dkrdj <109264979+dkrdj@users.noreply.github.com>
2023-03-21 12:44:57 +0900

    [Silver II] Title: 로또, Time: 84 ms, Memory: 12284 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            if (n == 0) {
+                System.out.println(sb);
+                return;
+            }
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+            comb(arr, 0, 0, new int[6]);
+            sb.append("\n");
+        }
+    }
+
+    private static void comb(int[] arr, int idx, int sidx, int[] res) {
+        if (idx == 6) {
+            for (int i = 0; i < 6; i++) {
+                sb.append(res[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = sidx; i < arr.length; i++) {
+            res[idx] = arr[i];
+            comb(arr, idx + 1, i + 1, res);
+        }
+    }
+
+}
+
