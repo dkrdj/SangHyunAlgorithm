00d2a4419a21c47c82337f293bf187be72b14d6b
dkrdj <109264979+dkrdj@users.noreply.github.com>
2023-04-04 22:21:50 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int area = brown + yellow; // ab
+        int sumLength = brown/2+2; //a+b
+        for(int i = 1 ; i <= sumLength/2;i++){
+            if(i * (sumLength-i) == area){
+                return new int[]{sumLength-i, i};
+            }
+        }
+        return null;
+    }
+}
