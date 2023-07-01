cb0d182d9859cfac374adb129edb97d634ce0b94
dkrdj <109264979+dkrdj@users.noreply.github.com>
2023-07-01 22:15:06 +0900

    [Silver IV] Title: 로프, Time: 184 ms, Memory: 20544 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,18 +1,22 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int[] arr = new int[10001];
         for (int i = 0; i < n; i++) {
-            pq.offer(Integer.parseInt(br.readLine()));
+            int num = Integer.parseInt(br.readLine());
+            arr[num]++;
         }
+        int rest = n;
         int answer = 0;
-        for (int i = 0; i < n; i++) {
-            answer = Math.max(answer, pq.poll() * (n - i));
+        for (int i = 1; i <= 10000; i++) {
+            if (arr[i] != 0) {
+                answer = Math.max(answer, i * rest);
+                rest -= arr[i];
+            }
         }
         System.out.println(answer);
     }
