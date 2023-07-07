c014f17ece6dcf586ff5f18559b403f610cd2031
dkrdj <109264979+dkrdj@users.noreply.github.com>
2023-07-08 02:20:19 +0900

    [Gold V] Title: 내려가기, Time: 320 ms, Memory: 47496 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.java
+++ 백준/Gold/2096. 내려가기/내려가기.java
@@ -15,25 +15,29 @@ public class Main {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int[][] max = new int[n + 1][3];
-        int[][] min = new int[n + 1][3];
-        max[1][0] = arr[1][0];
-        max[1][1] = arr[1][1];
-        max[1][2] = arr[1][2];
-        min[1][0] = arr[1][0];
-        min[1][1] = arr[1][1];
-        min[1][2] = arr[1][2];
+        int[] max = new int[3];
+        int[] min = new int[3];
+        max[0] = arr[1][0];
+        max[1] = arr[1][1];
+        max[2] = arr[1][2];
+        min[0] = arr[1][0];
+        min[1] = arr[1][1];
+        min[2] = arr[1][2];
+        int preMax0, preMax1, preMax2, preMin0, preMin1, preMin2;
         for (int i = 2; i <= n; i++) {
-            max[i][0] = Math.max(max[i - 1][0], max[i - 1][1]) + arr[i][0];
-            max[i][1] = Math.max(max[i - 1][0], Math.max(max[i - 1][1], max[i - 1][2])) + arr[i][1];
-            max[i][2] = Math.max(max[i - 1][1], max[i - 1][2]) + arr[i][2];
-            min[i][0] = Math.min(min[i - 1][0], min[i - 1][1]) + arr[i][0];
-            min[i][1] = Math.min(min[i - 1][0], Math.min(min[i - 1][1], min[i - 1][2])) + arr[i][1];
-            min[i][2] = Math.min(min[i - 1][1], min[i - 1][2]) + arr[i][2];
+            preMax0 = max[0];
+            preMax1 = max[1];
+            preMax2 = max[2];
+            preMin0 = min[0];
+            preMin1 = min[1];
+            preMin2 = min[2];
+            max[0] = Math.max(preMax0, preMax1) + arr[i][0];
+            max[1] = Math.max(preMax0, Math.max(preMax1, preMax2)) + arr[i][1];
+            max[2] = Math.max(preMax1, preMax2) + arr[i][2];
+            min[0] = Math.min(preMin0, preMin1) + arr[i][0];
+            min[1] = Math.min(preMin0, Math.min(preMin1, preMin2)) + arr[i][1];
+            min[2] = Math.min(preMin1, preMin2) + arr[i][2];
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append(Math.max(max[n][0], Math.max(max[n][1], max[n][2]))).append(" ").append(Math.min(min[n][0], Math.min(min[n][1], min[n][2])));
-        System.out.println(sb);
+        System.out.println(Math.max(max[0], Math.max(max[1], max[2])) + " " + Math.min(min[0], Math.min(min[1], min[2])));
     }
-
 }
