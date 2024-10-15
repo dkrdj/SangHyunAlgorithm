fbaeaa7c186d1563224f48a07a6f63be3d9377cf
dkrdj <109264979+dkrdj@users.noreply.github.com>
2024-10-16 00:40:22 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 252 ms, Memory: 68796 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        char[][] arr = new char[N][N];
+        star(arr, 0, 0, N);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(arr[i][j] == '*' ? '*' : ' ');
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void star(char[][] arr, int startR, int startC, int p) {
+        if (p == 1)
+            return;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 1 && j == 1) {
+                    continue;
+                }
+                if (p == 3)
+                    arr[startR + i][startC + j] = '*';
+                star(arr, startR + p / 3 * i, startC + p / 3 * j, p / 3);
+            }
+        }
+    }
+}
